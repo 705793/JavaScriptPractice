e060fb06de3b9f550a117668302dcc8a93370908
Ajinkya Tarlekar <80623907+705793@users.noreply.github.com>
2024-11-02 05:23:57 -0700

    Operation.js

--- comparision&Operation/Operation.js
+++ comparision&Operation/Operation.js
@@ -1,4 +1,4 @@
-console.log("1"+2);
-console.log(1+"2");
-console.log(1+2+"2");
-console.log("2"+1+2);
+console.log("1"+2); // result = 1 2
+console.log(1+"2"); // result = 1 2
+console.log(1+2+"2"); // result = 3 2
+console.log("2"+1+2); // result = 2 1 2
